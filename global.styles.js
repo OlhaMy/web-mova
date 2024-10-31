e7596ec1eced1f54ff9e5ba6c058bbb1885bf38c
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-10-31 21:51:57 +0100

    basic instalation

--- global.styles.js
+++ global.styles.js
@@ -2,6 +2,9 @@ const variablesStyles = '@import "./src/scss/common/_vars.scss"';
 const breakpointsStyles = '@import "./src/scss/common/_breakpoints.scss"';
 const mixinsStyles = '@import "./src/scss/common/_mixins.scss"';
 const resetStyles = '@import "./src/scss/common/_reset.scss"';
+const commonStyles = '@import "./src/scss/common/_common.scss"';
+const containerStyles = '@import "./src/scss/common/_container.scss"';
+const fontsStyles = '@import "./src/scss/common/_fonts.scss"';
 
 const styles = [
   variablesStyles,
@@ -10,6 +13,9 @@ const styles = [
   mixinsStyles,
   //   baseStyles,
   resetStyles,
+  commonStyles,
+  containerStyles,
+  fontsStyles,
 ];
 
 export const globalStylesOptions = styles.reduce((acc, i) => acc + i + ';', '');
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/header';
+import './js/home';
+import './js/features';
+import './js/play';
+import './js/faq';
+import './js/gallery';
+import './js/footer';
