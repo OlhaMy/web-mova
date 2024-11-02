cc2bcc02971dcadad9a1abeafc61bc82c14ffc5d
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-11-02 17:07:40 +0100

    section terms (html, scss), edited footer, privacy

--- src/js/footer.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const footerYearElement = document.querySelector('.footer-year');
-  if (footerYearElement) {
-    footerYearElement.textContent = new Date().getFullYear();
-  }
-});
