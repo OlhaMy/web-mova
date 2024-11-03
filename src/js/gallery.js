2d866270bf8fb7db58442b9fbeea318eb290d479
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-11-03 23:59:13 +0100

    section gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,24 +1,51 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+
 document.addEventListener('DOMContentLoaded', () => {
   const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 20,
+    modules: [Navigation, Pagination],
+    lazy: true,
+    slidesPerView: 1.2,
+    spaceBetween: 8,
     centeredSlides: true,
     loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    zoom: {
+      maxRatio: 2,
     },
+
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
+
     breakpoints: {
-      640: {
-        slidesPerView: 1,
-      },
-      1024: {
+      1200: {
         slidesPerView: 3,
+        spaceBetween: 16,
       },
     },
   });
+
+  // // Додаємо умову для контролю видимості кнопок навігації
+  // function updateNavigationVisibility() {
+  //   const prevButton = document.querySelector('.swiper-button-prev');
+  //   const nextButton = document.querySelector('.swiper-button-next');
+  //   if (window.innerWidth >= 1200) {
+  //     // Показуємо кнопки на десктопі
+  //     prevButton.style.display = 'flex';
+  //     nextButton.style.display = 'flex';
+  //   } else {
+  //     // Приховуємо кнопки на мобільних
+  //     prevButton.style.display = 'none';
+  //     nextButton.style.display = 'none';
+  //   }
+  // }
+
+  // Виконуємо функцію при завантаженні сторінки та при зміні розміру вікна
+  // updateNavigationVisibility();
+  // window.addEventListener('resize', updateNavigationVisibility);
 });
