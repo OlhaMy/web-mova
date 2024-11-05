b17d3f885bf60358fd516775cd69b0bb4d742e00
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-11-05 20:46:59 +0000

    fix gallery and some small other fixes

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,46 +1,49 @@
-import Swiper from 'swiper';
-import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
 import 'swiper/scss/effect-coverflow';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.swiper-container', {
-    modules: [Navigation, Pagination, EffectCoverflow],
-    effect: 'coverflow',
-    grabCursor: 'true',
-    centeredSlides: true,
-    initialSlide: 2,
-    preventClicks: true,
-    slidesPerView: 'auto',
-    lazy: true,
+const swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  initialSlide: 2,
+  preventClicks: true,
+  slidesPerView: 1.4,
+  spaceBetween: 8,
+  lazy: true,
 
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 250,
-      modifier: 1,
-      slideShadows: false,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 250,
+    modifier: 1,
+    slideShadows: false,
+  },
+  on: {
+    click(event) {
+      swiper.slideTo(swiper.clickedIndex);
     },
-    on: {
-      click(event) {
-        swiper.slideTo(swiper.clickedIndex);
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    1200: {
+      slidesPerView: 2.1,
+      spaceBetween: 10,
+      coverflowEffect: {
+        depth: 400,
+      },
+      navigation: {
+        prevEl: '.gallery-button-prev',
+        nextEl: '.gallery-button-next',
       },
     },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    breakpoints: {
-      1200: {
-        slidesPerView: 3,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      },
-    },
-  });
+  },
 });
